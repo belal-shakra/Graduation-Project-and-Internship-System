b5bc487d599cea12a1adc5440e464b3ddc5807af
belal-shakra <1b.shakra@gmail.com>
2024-06-12 23:51:50 +0300

    Reorganize App Routes

--- routes/web.php
+++ routes/web.php
@@ -34,54 +34,71 @@ require __DIR__.'/auth.php';
 
 
 ###########################################################
+########################## Login ##########################
+###########################################################
+
+
+Route::middleware('guest')->group(function(){
+    Route::get('/login', [StudentController::class, 'create'])->name('student.login');
+    Route::get('/supervisor/login', [SupervisorController::class, 'create'])->name('supervisor.login');
+    Route::get('/department/login', [DepartmentController::class, 'create'])->name('department.login');
+});
+
+
+
+
+###########################################################
 ######################### Student #########################
 ###########################################################
 
 
-// Main Student Routes
-Route::controller(StudentController::class)->name('student.')->group(function(){
-    Route::get('/login', 'create')->name('login');
-    Route::get('/', 'home')->name('home')->middleware('is.student');
-})->middleware('auth');
+Route::middleware(['auth', 'is.student'])->name('student.')->group(function(){
+    
+    
+    // Main Student Routes
+    Route::controller(StudentController::class)->group(function(){
+        Route::get('/', 'home')->name('home');
+    });
+    
+    
+    
+    
+    // Internship Company Routes
+    Route::middleware(['in.int'])->group(function(){
+        Route::get('/company/edit', [InternshipCompanyController::class, 'edit'])->name('company.edit');
+        Route::resource('company', InternshipCompanyController::class)->except(['index', 'show','edit']);
+    });
+    
+    
+    
+    // Internship Course Routes
+    Route::resource('course', InternshipCourseController::class)->middleware(['in.int'])
+    ->except(['index', 'show', 'edit']);
+    
+    
+    
+    // Graduation Project's Routes
+    Route::middleware(['in.gp'])->group(function(){
+        Route::get('/graduation-project/edit', [GraduationProjectController::class, 'edit'])->name('graduation-project.edit');
+        Route::resource('graduation-project', GraduationProjectController::class)->except(['index', 'show', 'edit']);
+
+        // Timeline's Route
+        Route::get('graduation-project/timeline',[TimelineController::class, 'index'])->name('timeline');
+    });
 
 
-
-
-// Internship Company Routes
-Route::middleware(['auth','in.int'])->group(function(){
-    Route::get('/company/edit', [InternshipCompanyController::class, 'edit'])->name('company.edit');
-    Route::resource('company', InternshipCompanyController::class)->except(['index', 'show','edit']);
 });
 
 
-
-// Internship Course Routes
-Route::resource('course', InternshipCourseController::class)->middleware(['auth','in.int'])
-->except(['index', 'show', 'edit']);
-
-
-
-
-// Graduation Project's Routes
-Route::middleware(['auth','in.gp'])->group(function(){
-    Route::get('/graduation-project/edit', [GraduationProjectController::class, 'edit'])->name('graduation-project.edit');
-    Route::resource('graduation-project', GraduationProjectController::class)->except(['index', 'show', 'edit']);
-});
-
-
-// Timeline's Route
-Route::get('graduation-project/timeline',[TimelineController::class, 'index'])->name('timeline')->middleware(['auth','in.gp']);
-
-
-// Graduation Project Post's Routes
-Route::middleware(['auth'])->group(function(){
+Route::middleware('auth')->group(function(){
+    
+    // Graduation Project Post's Routes
     Route::resource('post', PostController::class)->except(['index', 'create', 'show']);
+    
+    // Post's Comment Route
+    Route::post('/store/{post}', [CommentController::class, 'store'])->name('comment.store');
+    Route::resource('comment', CommentController::class)->only(['destroy']);
 });
-
-
-// Post's Comment Route
-Route::post('/store/{post}', [CommentController::class, 'store'])->middleware(['auth'])->name('comment.store');
-Route::resource('comment', CommentController::class)->only(['destroy'])->middleware(['auth']);
 
 
 
@@ -92,29 +109,33 @@ Route::resource('comment', CommentController::class)->only(['destroy'])->middlew
 ###########################################################
 
 
-// Supervisor's Main Route
-Route::controller(SupervisorController::class)->prefix('supervisor')->name('supervisor.')->group(function(){
-    Route::get('/login', 'create')->name('login');
-    Route::get('/', 'home')->name('home')->middleware('is.supervisor');
-})->middleware('auth');
+Route::middleware(['auth', 'is.supervisor'])->prefix('supervisor')->name('supervisor.')->group(function(){
+
+    // Supervisor's Main Route
+    Route::controller(SupervisorController::class)->group(function(){
+        Route::get('/', 'home')->name('home');
+    });
+    
+    
+    
+    // Supervisor's Graduation Project Route
+    Route::controller(SupervisorGraduationProjectController::class)->group(function(){
+        Route::get('/graduation-project-teams', 'index')->name('teams');
+        Route::get('/graduation-project-team-details', 'show')->name('show');
+    });
+    
+    
+    
+    // Supervisor's Internship Route
+    Route::controller(SupervisorInternshipController::class)->group(function(){
+        Route::get('/internship/student-list', 'index')->name('student-list');
+        Route::get('/internship/report/{user}', 'show')->name('report');
+        Route::post('/internship/report/store-course-note/{course}/{status}', 'storeCourseNote')->name('storeCourseNote');
+        Route::post('/internship/report/store-company-note/{company}/{status}', 'storeCompanyNote')->name('storeCompanyNote');
+    });
+});
 
 
-
-// Supervisor's Graduation Project Route
-Route::controller(SupervisorGraduationProjectController::class)->prefix('supervisor')->name('supervisor.')->group(function(){
-    Route::get('/graduation-project-teams', 'index')->name('teams');
-    Route::get('/graduation-project-team-details', 'show')->name('show');
-})->middleware(['auth','is.supervisor']);
-
-
-
-// Supervisor's Internship Route
-Route::controller(SupervisorInternshipController::class)->prefix('supervisor')->name('supervisor.')->group(function(){
-    Route::get('/internship/student-list', 'index')->name('student-list');
-    Route::get('/internship/report/{user}', 'show')->name('report');
-    Route::post('/internship/report/store-course-note/{course}/{status}', 'storeCourseNote')->name('storeCourseNote');
-    Route::post('/internship/report/store-company-note/{company}/{status}', 'storeCompanyNote')->name('storeCompanyNote');
-})->middleware(['auth','is.supervisor']);
 
 
 
@@ -127,21 +148,35 @@ Route::controller(SupervisorInternshipController::class)->prefix('supervisor')->
 ####################### Department ########################
 ###########################################################
 
-// Department's Main Route
-Route::controller(DepartmentController::class)->prefix('department')->name('department.')->group(function(){
-    Route::get('/', 'home')->name('home');
-    Route::get('/login', 'create')->name('login')->middleware('guest');
-    Route::post('/store/{department}', 'store')->name('store');
-})->middleware(['auth', 'is.head']);
+Route::middleware(['auth', 'is.head'])->prefix('department')->name('department.')->group(function(){
+    
+    
+    // Department's Main Route
+    Route::controller(DepartmentController::class)->group(function(){
+        Route::get('/', 'home')->name('home');
+        Route::post('/store/{department}', 'store')->name('store');
+    });
+    
+    
+    
+    // Department's Internship Route
+    Route::controller(DepartmentInternshipController::class)->group(function(){
+        Route::get('/in-internship-students', 'index')->name('in_int');
+        Route::post('/store', 'store')->name('store_in_int');
+        Route::get('/student-report/{student_rec}', 'show')->name('show');
+    });
+    
+    
+    
+    // Department's Graduation Project Route
+    Route::controller(DepartmentGraduationProjectController::class)->group(function(){
+        Route::get('/Graduation-Project/exceed-90-hours', 'exceed90')->name('exceed90');
+        Route::get('/Graduation-Project/teams', 'teams')->name('teams');
+        Route::get('/Graduation-Project/team-details/{graduation_project}', 'team_details')->name('team-details');
+    });
+});
 
 
-
-// Department's Internship Route
-Route::controller(DepartmentInternshipController::class)->prefix('department')->name('department.')->group(function(){
-    Route::get('/in-internship-students', 'index')->name('in_int');
-    Route::post('/store', 'store')->name('store_in_int');
-    Route::get('/student-report/{student_rec}', 'show')->name('show');
-})->middleware(['auth', 'is.head']);
 
 
 // Weekly Following Form
@@ -152,18 +187,10 @@ Route::controller(WeeklyFollowingFormController::class)->name('weekly.')->group(
 });
 
 
-
-// Department's Graduation Project Route
-Route::controller(DepartmentGraduationProjectController::class)->prefix('department')->name('department.')->group(function(){
-    Route::get('/Graduation-Project/exceed-90-hours', 'exceed90')->name('exceed90');
-    Route::get('/Graduation-Project/teams', 'teams')->name('teams');
-    Route::get('/Graduation-Project/team-details/{graduation_project}', 'team_details')->name('team-details');
-});
-
-
-
+###########################################################
+####################### Error Page ########################
+###########################################################
 
 
 Route::fallback(fn() => view('404'))->name('404');
-
 
